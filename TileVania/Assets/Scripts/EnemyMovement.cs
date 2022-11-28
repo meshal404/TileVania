aa4ff4e0224fc3c84b7667b6ce395708aeda3e78
ichigo <mshalalshawi@gmail.com>
2022-11-28 14:16:06 +0300

    update

---
+++ TileVania/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float enemySpeed = 1f;
+
+    Rigidbody2D enemyRb;
+    BoxCollider2D enemyBoxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+        enemyBoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.velocity = new Vector2(enemySpeed, enemyRb.velocity.y);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        enemySpeed = -enemySpeed;
+        transform.localScale = new Vector2(Mathf.Sign(enemySpeed) * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
--- TileVania/Assets/Scripts/Movement.cs
+++ TileVania/Assets/Scripts/Movement.cs
@@ -6,20 +6,25 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    Vector2 moveInput;
     Rigidbody2D rb2d;
     Animator animator;
-    bool playerHasHorizantalSpeed;
+    Vector2 moveInput;
     LayerMask layerMask;
-    CapsuleCollider2D collider;
+    bool playerHasHorizantalSpeed;
+    CircleCollider2D myFeetCollider;
+    CapsuleCollider2D myCapsuleCollider;
+
+    bool isAlive = true;
 
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] Vector2 deathBomb;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<CapsuleCollider2D>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<CircleCollider2D>();
         layerMask = LayerMask.GetMask("Ground");
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
@@ -28,14 +33,19 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isAlive) { return; }
         Run();
+        Death();
         FlipSprite();
-        animator.SetBool("isJumping", !collider.IsTouchingLayers(layerMask));
+        animator.SetBool("isJumping", !myFeetCollider.IsTouchingLayers(layerMask));
+        animator.SetBool("isAlive", isAlive);
     }
 
     void OnJump(InputValue value)
     {
-        if (value.isPressed && collider.IsTouchingLayers(layerMask))
+        if (!isAlive) { return; }
+
+        if (value.isPressed && myFeetCollider.IsTouchingLayers(layerMask))
         {
             rb2d.velocity += new Vector2(0f, jumpSpeed);
         }
@@ -43,11 +53,13 @@ public class Movement : MonoBehaviour
 
     void OnMove(InputValue value)
     {
+        if (!isAlive) { return; }
         moveInput = value.Get<Vector2>();
     }
 
     void Run()
     {
+        if (!isAlive) { return; }
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb2d.velocity.y);
         rb2d.velocity = playerVelocity;
 
@@ -56,9 +68,20 @@ public class Movement : MonoBehaviour
 
     void FlipSprite()
     {
+        if (!isAlive) { return; }
         playerHasHorizantalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
 
         if(playerHasHorizantalSpeed)
             transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x) * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+    }
+
+    void Death()
+    {
+        if (myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Enemies", "Water")))
+        {
+            isAlive = false;
+            rb2d.AddForce(new Vector2(deathBomb.x * -(Mathf.Sign(rb2d.velocity.x)), deathBomb.y), ForceMode2D.Impulse);
+        }
+            
     }
 }
